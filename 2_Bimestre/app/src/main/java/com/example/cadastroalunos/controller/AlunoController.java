f6627a54cf933ba16fe8c8effb7c781eeb56f6c1
Paulo Henrique Pereira dos Santos <84387138+paulohpssantos@users.noreply.github.com>
2024-11-06 19:52:30 -0300

    atualizaçao do projeto

--- 2_Bimestre/app/src/main/java/com/example/cadastroalunos/controller/AlunoController.java
+++ 2_Bimestre/app/src/main/java/com/example/cadastroalunos/controller/AlunoController.java
@@ -1,6 +1,8 @@
 package com.example.cadastroalunos.controller;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
 
 import com.example.cadastroalunos.dao.AlunoDao;
 import com.example.cadastroalunos.model.Aluno;
@@ -16,7 +18,43 @@ public class AlunoController {
     }
 
     public String salvarAluno(String ra, String nome){
-        return null;
+        try{
+            if(TextUtils.isEmpty(ra)){
+                return "Informe o RA do Aluno";
+            }
+            if(TextUtils.isEmpty(nome)){
+                return "Informe o NOME do Aluno";
+            }
+
+            //Verifica se existe um aluno cadastrado com
+            // o RA informado
+            Aluno aluno = AlunoDao
+                    .getInstance(context)
+                    .getById(Integer.parseInt(ra));
+
+            if(aluno != null){
+                return "O RA ("+ra+") já está cadastrado.";
+            }else{
+                aluno = new Aluno();
+                aluno.setRa(Integer.parseInt(ra));
+                aluno.setNome(nome);
+
+                long id = AlunoDao.getInstance(context).insert(aluno);
+                if(id > 0){
+                    return "Aluno cadastrado com sucesso!";
+                }else{
+                    return "Não foi possível gravar os dados do Aluno." +
+                            " Solicite suporte técnico.";
+                }
+            }
+
+
+        }catch (Exception ex){
+            Log.e("Unipar",
+                    "Erro salvarAluno(): "+ex.getMessage());
+        }
+        return "Erro ao gravar dados do Aluno. " +
+                "Solicite suporte técnico.";
     }
 
     public ArrayList<Aluno> retornarTodosAlunos(){
